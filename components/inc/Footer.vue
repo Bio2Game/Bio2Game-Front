46888f17012c0791fa7b2a2087903e6f3d35b7ec
DraftMan <nicovanaarsen@gmail.com>
2020-06-26 10:01:32 +0200

    Header & Footer created

---
+++ components/inc/Footer.vue
@@ -0,0 +1,177 @@
+<template>
+  <footer>
+    <div class="content">
+      <div class="items">
+        <div class="item a-propos">
+          <div class="title"><h5>A PROPOS</h5></div>
+          <p>
+            Un jeu qui vous permet de tester vos connaissances sur l’impact
+            environnemental de votre consommation et d'apprendre.
+          </p>
+          <nuxt-link to="/a-propos">En savoir plus</nuxt-link>
+        </div>
+        <div class="item contact">
+          <div class="title"><h5>CONTACT</h5></div>
+          <nuxt-link to="/charte-bio2game">La Charte Bio2Game</nuxt-link>
+          <nuxt-link to="/donnees-personnelles">
+            Politique de protection des données personnelles
+          </nuxt-link>
+          <nuxt-link to="/cookies">Utilisation des cookies</nuxt-link>
+        </div>
+        <div class="item social">
+          <div class="title"><h5>SOCIAL</h5></div>
+          <a class="contact-button" href="mailto:">Contactez nous</a>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      <p class="text">
+        Copyright Bio2Game 2018 © - All rights reserved - Développé par
+        <a href="https://www.draftman.fr">DraftMan</a>
+      </p>
+      <div class="scroll-top"></div>
+      <i class="icon-scroll-top"></i>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style lang="scss">
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  background: url('/images/footer.jpg');
+  background-size: cover;
+  background-position: center;
+  .content {
+    display: flex;
+    justify-content: center;
+    background-color: #212522c9;
+    padding: 40px;
+    width: 100%;
+    .items {
+      width: 100%;
+      max-width: 1150px;
+      margin: 0 auto;
+      display: flex;
+      .item {
+        flex: 1;
+        .title {
+          position: relative;
+          margin-bottom: 16px;
+          h5 {
+            padding-left: 15px;
+            margin: 0;
+            color: white;
+            &::after {
+              content: '';
+              position: absolute;
+              border-left: 3px solid #19683b;
+              height: 100%;
+              left: 2px;
+            }
+          }
+        }
+        p {
+          color: #ffffff;
+          font-size: 12px;
+          line-height: 20px;
+          width: 80%;
+          margin-bottom: 10px;
+        }
+        &.a-propos a {
+          text-decoration: none;
+          color: #27ae60;
+          text-transform: uppercase;
+          font-weight: 600;
+          font-size: 14px;
+        }
+        &.contact {
+          display: flex;
+          flex-direction: column;
+          a {
+            text-decoration: none;
+            color: #ffffff;
+            padding: 6px 0;
+            font-size: 13px;
+            transition: color 0.3s ease-in-out;
+            &:hover {
+              color: #27ae60;
+            }
+          }
+        }
+        &.social {
+          flex: none;
+        }
+        .contact-button {
+          color: #27ae60;
+          line-height: 34px;
+          border: 2px solid #27ae60;
+          border-radius: 30px;
+          padding: 8px 14px;
+          font-size: 11px;
+          text-transform: uppercase;
+          text-decoration: none;
+          font-weight: 600;
+          transition: all 0.3s ease-in-out;
+          &:hover {
+            background-color: #27ae60;
+            color: #3b413e;
+          }
+        }
+        @media screen and (max-width: 720px) {
+          margin-bottom: 40px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+        }
+      }
+      @media screen and (max-width: 720px) {
+        flex-direction: column;
+      }
+    }
+  }
+  .copyright {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #1a1d1be3;
+    width: 100%;
+    height: 50px;
+    .text {
+      color: #757575;
+      font-size: 14px;
+      padding: 0 20px;
+    }
+    .scroll-top {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      border-bottom: 90px solid #19683b;
+      border-left: 90px solid transparent;
+      & + i {
+        position: absolute;
+        bottom: 20px;
+        right: 15px;
+        z-index: 1;
+        color: #ffffff;
+        transition: opacity 0.3s;
+      }
+      &:hover + i,
+      & + i:hover {
+        opacity: 0.6;
+      }
+    }
+    @media screen and (max-width: 720px) {
+      justify-content: left;
+    }
+  }
+}
+</style>
---
+++ components/inc/Header.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="header" :class="{ 'fixed-header': isFixed }">
+    <div class="wrapper">
+      <div class="container menu">
+        <div class="left-menu">
+          <nuxt-link to="/">
+            <img class="logo" src="/images/logo.png" alt="Logo Bio2Game" />
+          </nuxt-link>
+          <nuxt-link class="link" to="/">Accueil</nuxt-link>
+          <nuxt-link class="link" to="/">Créer un quiz</nuxt-link>
+          <nuxt-link class="link" to="/domaines">Domaines</nuxt-link>
+          <nuxt-link class="link" to="/">Blog</nuxt-link>
+          <nuxt-link class="link" to="/">Communauté</nuxt-link>
+        </div>
+        <div class="right-menu">
+          <nuxt-link class="button md green" to="/register">
+            S’inscrire
+          </nuxt-link>
+          <nuxt-link class="button md gray right" to="/login">
+            Se connecter
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isFixed: false,
+      stage: false,
+    }
+  },
+  watch: {
+    $route(newRoute, oldRoute) {
+      if (newRoute.name !== 'index' && oldRoute.name === 'index') {
+        this.stage = true
+        setTimeout(() => {
+          this.stage = false
+        }, 600)
+      }
+      this.checkHeader()
+    },
+  },
+  mounted() {
+    this.checkHeader()
+    window.addEventListener('scroll', this.checkHeader)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.checkHeader)
+  },
+  methods: {
+    checkHeader() {
+      if (this.$route.name !== 'index') {
+        this.isFixed = true
+        return
+      }
+
+      const position = window.pageYOffset || document.documentElement.scrollTop
+      const isScrollingUp = this.isFixed ? position > 0 : position > 0
+
+      if (this.isFixed !== isScrollingUp) {
+        this.isFixed = isScrollingUp
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.header {
+  width: 100%;
+  .wrapper {
+    position: relative;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 5rem;
+    z-index: 20;
+    background-color: white;
+    transition: 0.1s background-color linear;
+    .menu {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left-menu {
+        display: flex;
+        align-items: center;
+        a .logo {
+          height: 4rem;
+          margin-top: -10px;
+        }
+        .link {
+          font-weight: 500;
+          font-size: 1rem;
+          line-height: 24px;
+          color: #4f4f4f;
+          margin: 0 0.8rem;
+          text-decoration: none;
+        }
+      }
+      .right-menu {
+        display: flex;
+      }
+    }
+  }
+  &.fixed-header {
+    height: 0;
+    animation: fadeOutBigger 0.3s ease-out forwards 0.4s;
+    @keyframes fadeOutBigger {
+      0% {
+        height: 0;
+      }
+      100% {
+        height: 5rem;
+      }
+    }
+    .wrapper {
+      transition: 0.1s background-color linear 0.3s;
+      position: fixed !important;
+      top: 0;
+      z-index: 1000;
+      opacity: 0;
+      height: 5rem;
+      animation: fadeIn 0.3s ease-out forwards 0.4s;
+      @keyframes fadeIn {
+        0% {
+          opacity: 0;
+          transform: translateY(-100px);
+        }
+        100% {
+          opacity: 1;
+          transform: translateY(0);
+        }
+      }
+    }
+  }
+}
+.route.index {
+  .header {
+    .wrapper {
+      background-color: transparent;
+      position: absolute;
+    }
+    &.fixed-header {
+      height: 0;
+      animation: none;
+      .wrapper {
+        background-color: white;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
+          0 1px 2px 0 rgba(0, 0, 0, 0.06);
+      }
+    }
+  }
+}
+</style>
