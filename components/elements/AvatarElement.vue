825205a5a77c42d43a4c98378c505d944c3dafb0
DraftMan <nicovanaarsen@gmail.com>
2020-07-01 01:53:19 +0200

    Avatar component created

---
+++ components/elements/AvatarElement.vue
@@ -0,0 +1,38 @@
+<template>
+  <img :src="getImage" alt="" :width="size" :height="size" />
+</template>
+
+<script>
+import md5 from 'md5'
+
+export default {
+  name: 'Avatar',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      default: '',
+    },
+    size: {
+      type: Number,
+      default: 16,
+    },
+  },
+  computed: {
+    getImage() {
+      return this.path
+        ? `/avatar_uploads/${this.path}`
+        : `https://www.gravatar.com/avatar/` +
+            md5(this.email.trim().toLowerCase()) +
+            `?s=${this.size}&d=mp`
+    },
+  },
+}
+</script>
