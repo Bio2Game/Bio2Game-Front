78657d444f6ac34852be9d4fb88386b653ab5720
DraftProducts <nicovanaarsen@gmail.com>
2020-09-07 19:40:40 +0200

    Login page update for mobile devices

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,12 +3,12 @@
     <div class="modal">
       <div class="overlay" :class="{ register: isRegister }">
         <div v-if="isRegister" class="content">
-          <h3>Vous avez déjà<br />un compte?</h3>
+          <h3>Vous avez déjà <br />un compte?</h3>
           <p>
             Cela signifie que vous êtes déjà des nôtres !<br />Connectez-vous
             dès maintenant à votre compte
           </p>
-          <a class="button green md equal" @click="isRegister = false"
+          <a class="button green md equal" @click="switchForm(false)"
             >Se connecter</a
           >
         </div>
@@ -18,81 +18,79 @@
             Créer votre compte gratuitement et commencez<br />dès maintenant à
             jouer à nos quizz
           </p>
-          <a class="button green md equal" @click="isRegister = true"
+          <a class="button green md equal" @click="switchForm(true)"
             >Créer un compte</a
           >
         </div>
       </div>
-      <div class="part register-part">
-        <h1>Créer un compte</h1>
-        <Input
-          v-model="username"
-          type="text"
-          placeholder="Pseudonyme"
-          :error="filtredErrors('username')"
-        />
-        <Input
-          v-model="email"
-          type="email"
-          placeholder="Email"
-          :error="filtredErrors('email')"
-        />
-        <Input
-          v-model="password"
-          type="password"
-          placeholder="Mot de passe"
-          :error="filtredErrors('password')"
-        />
-        <Input
-          v-model="password_confirmation"
-          type="password"
-          placeholder="Confirmez votre mot de passe"
-          :error="filtredErrors('password_confirmation')"
-        />
-        <a class="button green md full" @click="register()">Créer mon compte</a>
-      </div>
-      <div class="part login-part">
-        <h1>Connectez vous</h1>
-        <Input
-          v-model="email"
-          type="email"
-          placeholder="Email"
-          :error="filtredErrors('email')"
-        />
-        <Input
-          v-model="password"
-          type="password"
-          placeholder="Mot de passe"
-          :error="filtredErrors('password')"
-        />
-        <div class="password">
-          <nuxt-link to="/login/forgot">Mot de passe oublié ?</nuxt-link>
-        </div>
-
-        <div class="group">
-          <Checkbox
-            id="remember"
-            :checked="remember"
-            label="Se souvenir de moi"
+      <div class="parts" :class="{ wrap: !isRegister }">
+        <div class="part register-part">
+          <h1>Créer un compte</h1>
+          <Input
+            v-model="username"
+            type="text"
+            placeholder="Pseudonyme"
+            :error="filtredErrors('username')"
           />
-          <a class="button green sm" @click="login()">Connexion</a>
+          <Input
+            v-model="email"
+            type="email"
+            placeholder="Email"
+            :error="filtredErrors('email')"
+          />
+          <Input
+            v-model="password"
+            type="password"
+            placeholder="Mot de passe"
+            :error="filtredErrors('password')"
+          />
+          <a class="button green md full" @click="register()"
+            >Créer mon compte</a
+          >
         </div>
-        <hr />
-        <p class="description">Connectez vous avez vos réseaux sociaux</p>
+        <div class="part login-part">
+          <h1>Connectez vous</h1>
+          <Input
+            v-model="email"
+            type="email"
+            placeholder="Email"
+            :error="filtredErrors('email')"
+          />
+          <Input
+            v-model="password"
+            type="password"
+            placeholder="Mot de passe"
+            :error="filtredErrors('password')"
+          />
+          <div class="password">
+            <nuxt-link to="/login/forgot">Mot de passe oublié ?</nuxt-link>
+          </div>
 
-        <div class="social">
-          <a class="social-link facebook" href="/connexion/facebook">
-            <Facebook />
-          </a>
-          <a class="social-link google" href="/connexion/google">
-            <Google />
-          </a>
-          <a class="social-link twitter" href="/connexion/twitter">
-            <Twitter />
-          </a>
-          <a class="social-link likedin" href="/connexion/likedin">
-            <Linkedin />
-          </a>
+          <div class="group">
+            <Checkbox
+              id="remember"
+              :checked="remember"
+              label="Se souvenir de moi"
+            />
+            <a class="button green sm" @click="login()">Connexion</a>
+          </div>
+          <hr />
+          <p class="description">Connectez vous avez vos réseaux sociaux</p>
+
+          <div class="social">
+            <a class="social-link facebook" href="/login/facebook">
+              <Facebook />
+            </a>
+            <a class="social-link google" href="/login/google">
+              <Google />
+            </a>
+            <a class="social-link twitter" href="/login/twitter">
+              <Twitter />
+            </a>
+            <a class="social-link likedin" href="/login/likedin">
+              <Linkedin />
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -123,7 +121,6 @@ export default {
       username: '',
       email: '',
       password: '',
-      password_confirmation: '',
       errors: [],
       isRegister: false,
       remember: true,
@@ -132,6 +129,10 @@ export default {
   methods: {
     filtredErrors(field) {
       return this.errors.find((error) => error.field === field)
+    },
+    switchForm(value) {
+      this.isRegister = value
+      this.errors = []
     },
     async login() {
       try {
@@ -163,7 +164,6 @@ export default {
           username: this.username,
           email: this.email,
           password: this.password,
-          password_confirmation: this.password_confirmation,
         })
         await this.$auth.login({
           data: {
@@ -206,6 +206,9 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  @media screen and (max-width: $md) {
+    padding: 0 !important;
+  }
   .modal {
     width: 100%;
     max-width: 850px;
@@ -215,82 +218,103 @@ export default {
     border-radius: 8px;
     position: relative;
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.25);
-    .part {
-      display: flex;
-      flex: 1 0;
+    @media screen and (max-width: $md) {
+      margin: 0;
       flex-direction: column;
-      padding: 48px 32px;
-      h1 {
-        font-weight: bold;
-        font-size: 35px;
-        text-align: center;
-        color: $green;
-        margin-bottom: 30px;
+      overflow: hidden;
+      border-radius: 0;
+    }
+    .parts {
+      display: flex;
+      width: 100%;
+      max-width: 100%;
+      @media screen and (max-width: $md) {
+        overflow: hidden;
+        &.wrap .part {
+          transform: translateX(-100%);
+        }
       }
-      .input-field {
-        margin: 7px 0;
-      }
-      .group {
+      .part {
         display: flex;
-        label {
-          margin: auto 0;
-          flex: 1;
+        flex: 1 0;
+        flex-direction: column;
+        padding: 48px 32px;
+        transition: transform 0.3s ease-in-out;
+        @media screen and (max-width: $md) {
+          min-width: 100%;
         }
-        input[type='submit'] {
-          padding: 10px 14px;
+        h1 {
+          font-weight: bold;
+          font-size: 35px;
+          text-align: center;
+          color: $green;
+          margin-bottom: 30px;
         }
-      }
-      hr {
-        width: 100%;
-        margin-top: 24px;
-        border: 1px solid #999999;
-      }
-      .password {
-        display: flex;
-        justify-content: flex-end;
-        margin: -6px 0 16px;
-        a {
-          color: #19683b;
-          font-size: 14px;
-          text-decoration: none;
-          font-weight: 500;
+        .input-field {
+          margin: 7px 0;
         }
-      }
-      .description {
-        text-align: center;
-        margin-top: 16px;
-        font-size: 14px;
-        color: #999999;
-      }
-      .social {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(70px, 1fr));
-        gap: 12px;
-        margin-top: 26px;
-        .social-link {
+        .group {
           display: flex;
-          align-items: center;
-          justify-content: center;
-          flex: 1;
-          color: #ffffff;
-          height: 52px;
-          font-size: 60px;
-          border-radius: 6px;
-          svg {
-            width: 28px;
+          label {
+            margin: auto 0;
+            flex: 1;
+          }
+          input[type='submit'] {
+            padding: 10px 14px;
           }
         }
-        .facebook {
-          background-color: #3b5998;
+        hr {
+          width: 100%;
+          margin-top: 24px;
+          border: 1px solid #999999;
         }
-        .google {
-          background-color: #dd4b39;
+        .password {
+          display: flex;
+          justify-content: flex-end;
+          margin: -6px 0 16px;
+          a {
+            color: #19683b;
+            font-size: 14px;
+            text-decoration: none;
+            font-weight: 500;
+          }
         }
-        .twitter {
-          background-color: #00acee;
+        .description {
+          text-align: center;
+          margin-top: 16px;
+          font-size: 14px;
+          color: #999999;
         }
-        .likedin {
-          background-color: #0e76a8;
+        .social {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(70px, 1fr));
+          gap: 12px;
+          margin-top: 26px;
+          .social-link {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex: 1;
+            color: #ffffff;
+            height: 52px;
+            font-size: 60px;
+            border-radius: 6px;
+            svg {
+              width: 28px;
+            }
+          }
+          .facebook {
+            background-color: #3b5998;
+          }
+          .google {
+            background-color: #dd4b39;
+          }
+          .twitter {
+            background-color: #00acee;
+          }
+          .likedin {
+            background-color: #0e76a8;
+          }
         }
       }
     }
@@ -315,8 +339,24 @@ export default {
       border-radius: 16px;
       box-shadow: 0 3px 16px rgba(0, 0, 0, 0.5);
       transition: transform 0.3s ease-in-out;
+
+      @media screen and (max-width: $md) {
+        position: relative;
+        top: 0;
+        left: 0;
+        width: 100%;
+        padding: 24px;
+        border-radius: 0 0 16px 16px;
+        br {
+          display: none;
+        }
+      }
+
       &.register {
         transform: translateX(100%);
+        @media screen and (max-width: $md) {
+          transform: translate(0);
+        }
       }
       .content {
         display: flex;
@@ -328,10 +368,14 @@ export default {
           color: #ffffff;
           line-height: 40px;
           margin-bottom: 10px;
+          @media screen and (max-width: $md) {
+            font-size: 24px;
+            line-height: 32px;
+          }
         }
         p {
-          font-size: 11px;
-          line-height: 13px;
+          font-size: 12px;
+          line-height: 14px;
           color: #999999;
           margin-bottom: 15px;
         }
