746e71eece67976cdf36283d705ae21f3d975ec9
DraftProducts <nicovanaarsen@gmail.com>
2020-10-05 20:17:35 +0200

    page redirection to quizzes added

---
+++ pages/contributeur/quizzes/_quiz_ref/questions/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  middleware({ params, redirect }) {
+    redirect(301, `/contributeur/quizzes/${params.quiz_ref}`)
+  },
+}
+</script>
