0ff9b017057c90185a82e0183226f19616f5bac5
DraftProducts <nicovanaarsen@gmail.com>
2020-10-21 11:26:20 +0200

    SocketIO connect updated

--- plugins/vue-socket.io.js
+++ plugins/vue-socket.io.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import io from 'socket.io-client'
 import VueSocketIOExt from 'vue-socket.io-extended'
 
-const socket = io({ path: '/socket' })
+const socket = io({ path: '/socket', autoConnect: false })
 
 export default ({ store }) => {
   Vue.use(VueSocketIOExt, socket, {
