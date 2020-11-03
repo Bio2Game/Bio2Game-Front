a2c8646e2f1bcbd00c772b4eac95fd42660c4a77
DraftProducts <nicovanaarsen@gmail.com>
2020-11-03 15:59:00 +0100

    Error page created

---
+++ components/parties/NotFound.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="not-found">
+    <div class="head">
+      <h1>
+        <img
+          src="../../assets/images/logo.png"
+          alt="Logo Bio2Game"
+          title="Bio2Game"
+        />
+        <div v-html="getCode(error.error)" />
+      </h1>
+      <h4>{{ error.message }}</h4>
+    </div>
+    <div class="buttons">
+      <nuxt-link to="/parties" class="button green lg">
+        Changer de partie
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    getCode(code) {
+      const caracters = code.toString().split('')
+      return caracters
+        .map((c, index) => (index % 2 ? `<span>${c}</span>` : c))
+        .join('')
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.not-found {
+  margin: 20vh auto 0;
+  max-width: 600px;
+  .head {
+    text-align: center;
+    h1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 160px;
+      font-weight: 900;
+      color: $green;
+      margin-bottom: 0;
+      user-select: none;
+
+      span {
+        color: #ffffff;
+      }
+      img {
+        height: 150px;
+      }
+
+      @media screen and (max-width: 720px) {
+        font-size: 120px;
+        img {
+          height: 130px;
+        }
+      }
+    }
+    h4 {
+      font-size: 26px;
+      margin: 5px 0 20px;
+      color: #272727;
+      @media screen and (max-width: 720px) {
+        font-size: 20px;
+      }
+    }
+  }
+  .buttons {
+    display: flex;
+    justify-content: center;
+    .button {
+      max-width: 100%;
+      width: 100%;
+    }
+  }
+}
+</style>
