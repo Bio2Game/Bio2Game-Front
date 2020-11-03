e3ff703ab754afebf81a0e316047648faf04c68c
DraftProducts <nicovanaarsen@gmail.com>
2020-11-03 15:58:37 +0100

    Tabs component created

---
+++ components/elements/tabs/Tab.vue
@@ -0,0 +1,36 @@
+<template>
+  <section v-show="active" :id="id" class="tab-content">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    hidden: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      active: false,
+    }
+  },
+  watch: {
+    hidden(value) {
+      if (value && this.active) {
+        this.$parent.selectTab(this.$parent.lastActiveTab, null)
+      }
+    },
+  },
+}
+</script>
---
+++ components/elements/tabs/Tabs.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="tabs">
+    <ul class="tabs-flags">
+      <li
+        v-for="tab in tabs"
+        v-show="!tab.hidden"
+        :key="tab.id"
+        :class="{ active: tab.active }"
+        class="tab-name"
+        @click="$emit('input', tab.id)"
+      >
+        {{ tab.name }}
+      </li>
+    </ul>
+    <div class="tabs-contents">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, null],
+      default: '',
+    },
+  },
+  data() {
+    return {
+      lastActiveTab: '',
+      tabs: [],
+    }
+  },
+  watch: {
+    value(value, oldValue) {
+      if (value !== oldValue) {
+        this.selectTab(value, oldValue)
+      }
+    },
+  },
+  created() {
+    this.tabs = this.$children
+  },
+  mounted() {
+    this.selectTab(this.value, null)
+  },
+  methods: {
+    selectTab(selectedTabId, oldValue) {
+      const selectedTab = this.tabs.find(tab => tab.id === selectedTabId)
+      if (!selectedTab) {
+        if (!this.tabs.length) return
+        return this.selectTab(this.tabs[0].id)
+      }
+      this.tabs.forEach(tab => {
+        tab.active = tab.id === selectedTab.id
+      })
+      this.lastActiveTab = oldValue
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.tabs {
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+  .tabs-flags {
+    display: flex;
+    .tab-name {
+      border-radius: 8px;
+      transition: 0.2s background-color ease-in-out;
+      cursor: pointer;
+      user-select: none;
+      box-shadow: 0 1px 6px rgba(0, 0, 0, 0.25);
+      padding: 16px 24px;
+      color: white;
+      margin: 0 24px 24px 0;
+      background-color: $green;
+      &.active {
+        background-color: $green-light;
+      }
+    }
+  }
+  .tabs-contents {
+    background-color: white;
+    border-radius: 8px;
+    padding: 24px;
+    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.25);
+  }
+}
+</style>
