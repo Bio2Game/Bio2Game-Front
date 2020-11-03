1221ddf4f0bebd098a04e7355e0b93c56fbdb64e
DraftProducts <nicovanaarsen@gmail.com>
2020-11-03 16:00:00 +0100

    stats goughnut component created for stats

---
+++ components/parties/elements/DoughnutElement.vue
@@ -0,0 +1,38 @@
+<script>
+import Chart from 'chart.js'
+import { Doughnut, mixins } from 'vue-chartjs'
+
+import 'chartjs-plugin-labels'
+
+export default {
+  extends: Doughnut,
+  mixins: [mixins.reactiveProp],
+  data() {
+    return {
+      options: {
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        cutoutPercentage: 65,
+        tooltips: {
+          callbacks: {
+            label({ index }, { labels, datasets }) {
+              return ` ${datasets[0].data[index]} ${labels[index]}`
+            },
+          },
+        },
+      },
+    }
+  },
+  mounted() {
+    Chart.defaults.global.plugins.labels = {
+      render: 'percentage',
+      fontSize: 15,
+      fontColor: '#fff',
+      overlap: false,
+    }
+    this.renderChart(this.chartData, this.options)
+  },
+}
+</script>
