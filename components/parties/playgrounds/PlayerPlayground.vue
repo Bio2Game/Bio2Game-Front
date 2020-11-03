8c82b667b3590c67547ced49ce66201f37dad6ef
DraftProducts <nicovanaarsen@gmail.com>
2020-11-03 16:00:36 +0100

    Player playground component created

---
+++ components/parties/playgrounds/PlayerPlayground.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="playground user-playground block">
+    <div class="head">
+      <h5>Partie</h5>
+      Statu: {{ game.status }}
+    </div>
+    <div class="content">
+      <WaitingState v-if="!game.status" />
+      <QuestionState v-else-if="game.status !== 3" />
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'PlayerPlayground',
+  computed: mapState('parties', ['game']),
+}
+</script>
