e5847e8d1759e6985119d70d5d5c12e430788b0c
DraftProducts <nicovanaarsen@gmail.com>
2020-11-17 14:36:52 +0100

    style(ux): :lipstick: Update global static styles

--- pages/quizzes/_quiz.vue
+++ pages/quizzes/_quiz.vue
@@ -42,7 +42,7 @@ export default {
       max-width: 700px;
       margin: 0 auto;
       .infos {
-        margin-bottom: 25px;
+        margin-bottom: 32px;
         .title {
           display: flex;
           align-items: flex-end;
@@ -57,18 +57,14 @@ export default {
           p {
             color: #484848;
             text-align: center;
-            font-weight: 600;
             font-size: 16px;
             margin-top: -8px;
-            margin-bottom: 16px;
+            margin-bottom: 8px;
           }
           .question-position {
             font-size: 18px;
             font-weight: 600;
             margin-bottom: 24px;
-            .total {
-              color: $green;
-            }
           }
         }
         .progress-bar {
@@ -111,7 +107,7 @@ export default {
           user-select: none;
           border-radius: 3px;
           background-color: #f8f7f7;
-          border: 5px solid #f8f7f7;
+          border: 6px solid #f8f7f7;
           transition: 0.3s background-color ease-out, 0.3s border-color ease-out;
           cursor: pointer;
           @media screen and (max-width: 480px) {
@@ -140,8 +136,21 @@ export default {
           }
           &.checked {
             background-color: #c5c5c5;
-            border-color: #c5c5c5;
+            border-color: #ffffff;
           }
+        }
+      }
+      .explication {
+        margin-top: 8px;
+        color: #585858;
+        font-size: 15px;
+        .explication-title {
+          margin-bottom: 4px;
+          font-weight: 600;
+        }
+        .source {
+          color: $green;
+          margin: 12px auto 0;
         }
       }
       &.question-interface {
@@ -150,7 +159,7 @@ export default {
           color: #414141;
           background-color: #f8f7f7;
           padding: 16px;
-          margin-bottom: 16px;
+          margin-bottom: 24px;
           user-select: none;
           .question-content {
             width: calc(100% - 100px);
@@ -163,6 +172,19 @@ export default {
             img {
               max-width: 100%;
             }
+          }
+        }
+        .explication {
+          background: #ffffff;
+          padding: 16px;
+          box-shadow: 0 4px 5px rgba(0, 0, 0, 0.1);
+        }
+        .submit-next {
+          margin-top: 8px;
+          width: 100%;
+          max-width: 100%;
+          &.resp {
+            margin-top: 24px;
           }
         }
       }
@@ -230,15 +252,66 @@ export default {
                 padding: 24px;
                 .responses .item {
                   cursor: default;
-                }
-                .explication {
-                  margin-top: 16px;
-                  color: #585858;
-                  .source {
-                    color: $green;
-                    margin: 12px auto 0;
+                  &.checked {
+                    border-color: #c5c5c5;
                   }
                 }
+              }
+            }
+          }
+        }
+        .results {
+          background: #ffffff;
+          box-shadow: 0 4px 5px rgba(0, 0, 0, 0.1);
+          border-radius: 4px;
+          margin-top: 16px;
+          /* stylelint-disable-next-line */
+          .head {
+            display: flex;
+            justify-content: center;
+            color: white;
+            box-shadow: 0 4px 5px rgba(0, 0, 0, 0.1);
+            border-radius: 3px 3px 0 0;
+            padding: 12px 16px;
+            background-color: $green;
+          }
+          /* stylelint-disable-next-line */
+          .content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 24px;
+            .stats {
+              display: flex;
+              width: 100%;
+              padding: 8px 0 32px;
+              .stat {
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+                flex: 1;
+                .num {
+                  font-size: 64px;
+                  font-weight: 900;
+                  margin-bottom: 8px;
+                  &.good {
+                    color: $green;
+                  }
+                  &.bad {
+                    color: #f43434;
+                  }
+                }
+                .label {
+                  font-size: 16px;
+                }
+              }
+            }
+            .submit-next {
+              margin-top: 8px;
+              width: 100%;
+              max-width: 400px;
+              &.resp {
+                margin-top: 24px;
               }
             }
           }
