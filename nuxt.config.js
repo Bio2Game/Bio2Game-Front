ac0d1226ef7206126c912711caaa607e91fbb038
DraftMan <nicovanaarsen@gmail.com>
2022-01-18 11:43:46 +0100

    chore: ➕ Add body-scroll-lock

--- nuxt.config.js
+++ nuxt.config.js
@@ -138,6 +138,7 @@ export default {
   plugins: [
     { src: '~/plugins/vue-carousel.js', mode: 'client' },
     { src: '~/plugins/vue-notification.js', mode: 'client' },
+    { src: '~/plugins/vue-body-scroll-lock', mode: 'client' },
     { src: '~/plugins/vue-closable.js', mode: 'client' },
     { src: '~/plugins/vue-socket.io.js', mode: 'client' },
   ],
