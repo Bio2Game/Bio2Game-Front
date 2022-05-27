25cddc136ec6f09af5febe2b96c61ce6980239b0
DraftMan <nicovanaarsen@gmail.com>
2022-05-27 05:02:16 +0200

    feat: ✨ Add petit-bateau page

--- pages/petit-bateau.vue
+++ pages/petit-bateau.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container petit-bateau-event">
-    <div class="wrapper">
+    <div v-if="!status && !askAuth" class="wrapper">
       <div class="logo">
         <PetitBateau />
       </div>
@@ -12,17 +12,94 @@
         saepe incidunt officiis ipsa.
       </div>
       <div class="languages">
-        <div class="lang"><LangFR /></div>
-        <div class="lang"><LangEN /></div>
-        <div class="lang"><LangDE /></div>
-        <div class="lang"><LangIT /></div>
-        <div class="lang"><LangMAR /></div>
+        <div class="lang" @click="play('fr')">
+          <LangFR />
+          <div class="overlay">Jouer en français</div>
+        </div>
+        <div class="lang" @click="play('en')">
+          <LangEN />
+          <div class="overlay">Play in english</div>
+        </div>
+        <div class="lang" @click="play('de')">
+          <LangDE />
+          <div class="overlay">Auf deutsch spielen</div>
+        </div>
+        <div class="lang" @click="play('it')">
+          <LangIT />
+          <div class="overlay">Gioca in italiano</div>
+        </div>
+        <div class="lang" @click="play('mar')">
+          <LangMAR />
+          <div class="overlay">العب بالعربية</div>
+        </div>
       </div>
     </div>
+    <div v-else-if="status" class="container">
+      <section class="playground">
+        <QuizQuestionState v-if="[1, 2].includes(status)" />
+        <QuizResultState v-if="status === 3" />
+      </section>
+    </div>
+    <section v-else class="simpleAuth">
+      <div class="block">
+        <div class="head">
+          <h5>Identification</h5>
+        </div>
+        <div class="content">
+          <p v-if="lang === 'fr'">
+            Afin de vous distinguer des autres joueurs dans la partie, veuillez
+            remplir ces informations.
+          </p>
+          <p v-if="lang === 'en'">
+            In order to distinguish yourself from other players in the game,
+            please fill in this information.
+          </p>
+          <p v-if="lang === 'de'">
+            Um sich von anderen Spielern im Spiel abzuheben, füllen Sie bitte
+            diese Informationen aus.
+          </p>
+          <p v-if="lang === 'it'">
+            Per distinguerti dagli altri giocatori nel gioco, compila queste
+            informazioni.
+          </p>
+          <p v-if="lang === 'mar'">
+            من أجل تمييز نفسك عن اللاعبين الآخرين في اللعبة ، يرجى ملء هذه
+            المعلومات.
+          </p>
+          <InputElement
+            :value="firstname"
+            type="text"
+            placeholder="First name"
+            @input="firstname = $event"
+          />
+          <InputElement
+            :value="lastname"
+            type="text"
+            placeholder="Last name"
+            @input="lastname = $event"
+          />
+          <InputElement
+            :value="website"
+            type="text"
+            placeholder="Website"
+            @input="website = $event"
+          />
+          <button
+            class="button green lg"
+            :class="{ disabled: !firstname || !lastname || !website }"
+            @click="registerSimpleAuth()"
+          >
+            Start quiz !
+          </button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import PetitBateau from '@/assets/images/petit-bateau-event.svg?inline'
 
 import LangFR from '@/assets/images/lang/fr.svg?inline'
@@ -42,6 +119,64 @@ export default {
     LangIT,
     LangMAR,
   },
+  data() {
+    return {
+      firstname: '',
+      lastname: '',
+      website: '',
+      askAuth: false,
+      lang: null,
+      quizzesLangs: {
+        fr: 1,
+        en: 2,
+        de: 3,
+        it: 4,
+        mar: 5,
+      },
+      started: false,
+    }
+  },
+  computed: mapState('quiz', ['status']),
+  destroyed() {
+    this.$store.commit('quiz/RESET_STATE')
+  },
+  methods: {
+    async play(lang) {
+      if (this.lang) return
+
+      this.lang = lang
+
+      if (!this.$auth.loggedIn) {
+        this.askAuth = true
+      }
+
+      try {
+        await this.$store.dispatch('quiz/fetchQuiz', {
+          id: this.quizzesLangs[this.lang],
+          type: 'quiz',
+        })
+      } catch (error) {
+        console.error(error)
+        return this.$notify({
+          type: 'error',
+          text: "Une erreur s'est produite lors du chargement du quiz.",
+          duration: 3000,
+          width: 400,
+        })
+      }
+    },
+    async registerSimpleAuth() {
+      if (!this.firstname || !this.lastname || !this.website) return
+
+      await this.$auth.loginWith('guest', {
+        data: {
+          username: `${this.firstname}:${this.lastname}:${this.website}`,
+        },
+      })
+
+      this.askAuth = false
+    },
+  },
 }
 </script>
 
@@ -52,7 +187,6 @@ export default {
   .wrapper {
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
     width: 100%;
     padding: 70px 16px;
@@ -91,28 +225,85 @@ export default {
   }
 
   .languages {
-    display: grid;
+    display: flex;
+    justify-content: center;
     gap: 32px;
-    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-    @media screen and (max-width: 1028px) {
-      // grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    }
-
+    flex-wrap: wrap;
     max-width: 1200px;
 
     .lang {
-      border-radius: 5px;
       position: relative;
+      display: flex;
+      width: 200px;
+      border: 5px solid $green;
       cursor: pointer;
+      border-radius: 5px;
+      filter: drop-shadow(0 3px 6px rgba(0, 0, 0, 0.25));
+      min-width: 200px;
+      transition: transform 0.3s ease;
 
       svg {
         width: 100%;
         height: 100%;
       }
 
+      .overlay {
+        position: absolute;
+        top: -1px;
+        right: -1px;
+        bottom: -1px;
+        left: -1px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: $green;
+        color: white;
+        opacity: 0;
+        font-weight: 600;
+        transition: opacity 0.3s ease;
+      }
+
+      &:hover {
+        transform: scale(1.1);
+
+        // .overlay {
+        //   opacity: 1;
+        //   transition: opacity 0.3s ease 0.6s;
+        // }
+      }
+
       // .placeholder {
       //   padding-bottom: 65.4296875%;
       // }
+    }
+  }
+
+  .simpleAuth {
+    max-width: 500px;
+    margin: auto;
+
+    .block {
+      margin-top: 0;
+      // stylelint-disable-next-line no-descending-specificity
+      .content {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        // stylelint-disable-next-line no-descending-specificity
+        p {
+          line-height: 24px;
+          margin-bottom: 16px;
+        }
+
+        .input-container {
+          width: 100%;
+        }
+        // stylelint-disable-next-line no-descending-specificity
+        .button {
+          width: 100%;
+          max-width: 100%;
+        }
+      }
     }
   }
 }
