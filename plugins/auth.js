4bdc2fb15129f05f1345d6c02290af89a517eb10
DraftMan <nicovanaarsen@gmail.com>
2022-06-01 14:37:14 +0200

    fix: 🐛 Fix petit-bateau auth middleware

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,6 +1,6 @@
 export default function ({ $auth }) {
   $auth.onRedirect((_to, from) => {
-    if (/^\/parties\/[A-Za-z1-9]{8}$|^\/quiz\/(\d+)-\S+$/.test(from)) {
+    if (/^\/parties\/[A-Za-z1-9]{8}$|^\/quiz\/(\d+)-\S+$|^\/petit-bateau$/.test(from)) {
       return from
     }
   })
